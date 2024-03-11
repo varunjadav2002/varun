dcab87a0a85e245464f7c6beb21f26dcbe405476
varunjadav2002 <155519638+varunjadav2002@users.noreply.github.com>
2024-03-11 12:58:58 +0530

    Add files via upload

---
+++ triangle..py
@@ -0,0 +1,6 @@
+base = float(input("length of the base of the triangle:"))
+height = float(input("hight of the triangle:"))
+
+area = 0.5 * base * height
+
+print("area of the triangle whith base and height",area)
